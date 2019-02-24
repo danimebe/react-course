8d7ece189889daf71f930277f6f260b47133d1e9
danimebe <danielmb0515@gmail.com>
2019-02-23 22:30:12 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app');
+
+/* const holaMundo = <h1>Hola mundo!!</h1>; */
+ReactDOM.render(<Media/>,app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+class Media extends React.Component {
+      render(){
+          return (
+            <div>
+                <div>
+                    <img src="" alt="" width={260} height={160} >
+                    </img>
+                    <h3>¿Por qué aprender React?</h3>
+                    <p>Daniel Mejía Benjumea</p>
+                </div>
+            </div>
+          )
+      }
+}
+
+export default Media;
